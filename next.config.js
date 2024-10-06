71c96effa3c0dbc3d31947e1c1349c7814d77d21
Andres Felipe Rodriguez roa <89116331+japipe05@users.noreply.github.com>
2024-10-06 11:45:27 -0500

    .

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /**
 * @type {import('next').NextConfig}
 */
+let basePath = '';
+
+if (process.env.APP_ENTORNO === 'PRODUCCION') {
+  basePath = process.env.NEXT_PUBLIC_BASE_PATH;
+}
+
 const nextConfig = {
-    basePath: '/mssinergy',
+    basePath: basePath,
     output: 'export',
     distDir: 'dist',
     images:{
