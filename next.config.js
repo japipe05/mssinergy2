999cf12e361dee5d958d0e86041e8a8f8d0af6e9
Andres Felipe Rodriguez roa <89116331+japipe05@users.noreply.github.com>
2024-10-06 11:27:17 -0500

    .

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 * @type {import('next').NextConfig}
 */
 const nextConfig = {
-    basePath: process.env.NEXT_PUBLIC_BASE_PATH || null,
-    assetPrefix: process.env.NEXT_PUBLIC_IMAGE_PATH || null,
+    basePath: "/mssinergy",
+    assetPrefix: "/mssinergy/images",
     output: 'export',
     distDir: 'dist',
     images:{
