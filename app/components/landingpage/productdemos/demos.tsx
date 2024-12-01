f2d53504cd7b337c780b23878e330a6fc33ecf42
Andres Felipe Rodriguez roa <89116331+japipe05@users.noreply.github.com>
2024-12-01 18:25:49 -0500

    segunda fase

--- app/components/landingpage/productdemos/demos.tsx
+++ app/components/landingpage/productdemos/demos.tsx
@@ -1,21 +1,53 @@
 import React from "react";
-import AllDemos from "./AllDemos";
-
+import Image from "next/image";
+import bannerWidget1 from "/public/images/landingpage/background/mision.jpg";
+import { useTranslation } from "react-i18next";
 
 const ProductDemos = () => {
-
+  const { t } = useTranslation();
   return (
     <>
       <div className="md:py-20 py-12 relative dark:bg-dark" id="demos">
         <div className="container">
-          <div className=" lg:w-8/12 w-full" data-aos="fade-up" data-aos-duration="500">
-            <p className="text-body text-sm font-medium dark:text-darkmuted" >What’s included?</p>
-            <h2 className=" sm:text-[30px] text-2xl font-bold leading-10 mt-1">
-            6 multipurpose and easily customizable demo variations included in the package
-            </h2>
+          <div className="flex flex-wrap items-center">
+            {/* Imagen */}
+            <div
+              className="w-full md:w-5/12 mb-6 md:mb-0"
+              data-aos="fade-right"
+              data-aos-duration="500"
+            >
+              <Image
+                src={bannerWidget1}
+                alt="Misión Ms Sinergy"
+                className="rounded-lg shadow-lg"
+              />
+            </div>
+            {/* Texto */}
+            <div
+              className="w-full md:w-7/12 pl-16 pr-16" // Incrementado el espacio a 12 unidades
+              data-aos="fade-up"
+              data-aos-duration="500"
+            >
+              <h1
+                className="text-[36px] leading-none font-bold text-primary mb-4"
+                data-aos="fade-up"
+                data-aos-delay="200"
+                data-aos-duration="1000"
+              >
+                Misión
+              </h1>
+              <p
+                className="text-justify leading-7 text-lg font-medium text-body dark:text-darkmuted"
+                data-aos="fade-up"
+                data-aos-delay="300"
+                data-aos-duration="1100"
+              >
+                {t(
+                  "Ms Sinergy SAS, es una organización que direcciona las áreas de metrología (magnitud de presión) y ensayos, en los sectores industriales, farmacéutico, alimentario, hospitalario y veterinario, manteniéndose a la vanguardia de las nuevas tecnologías a través de la implementación de metodologías ágiles con base a una mejora continua en los procesos generando la satisfacción y desarrollo sostenible de sus clientes"
+                )}
+              </p>
+            </div>
           </div>
-          {/* Demos */}
-          <AllDemos />
         </div>
       </div>
     </>
--- app/components/landingpage/ticket/Ticket.tsx
+++ app/components/landingpage/ticket/Ticket.tsx
@@ -3,20 +3,22 @@ import Image from "next/image";
 import banner from "/public/images/landingpage/background/business-woman-checking-her-mail.png";
 import { Button } from "flowbite-react";
 import Link from "next/link";
-
+import { useTranslation } from "react-i18next";
 const Ticket = () => {
+  const { t } = useTranslation();
   return (
     <div className="bg-primary dark:bg-primary">
       <div className="container ">
        <div className="flex justify-between relative py-12">
        <div className="lg:w-5/12 w-full lg:pt-12 px-2">
             <h5 className="text-3xl leading-8 font-medium text-white text-center lg:text-start mb-[30px]">
-              Build your app with our highly customizable Flowbite Tailwind based Dashboard
+            {t(
+                  "Politica de Calidad")}
             </h5>
-            <div className="sm:flex items-center gap-3 pb-16">
-                    <Link href="/auth/auth1/login" target="_blank" className=" py-3 rounded-full text-primary block bg-white px-7 border border-primary font-semibold" type="button">Login</Link>
-                    <Link href="/auth/auth1/register" target="_blank" className="border border-white rounded-full d-block px-7 py-3 text-white font-semibold " type="button">Register</Link>
-            </div>
+            <p className="leading-7 text-lg font-medium text-white dark:text-darkmuted">
+            {t(
+                  "Ms Sinergy S.A.S., somos un laboratorio altamente comprometido con la calidad y la excelencia en nuestros servicios. Nos acogemos a las metodologías como: BPM, BPL, BPA Y BPD para garantizar la satisfacción total de nuestros clientes. Adoptamos un enfoque de mejora continua, buscando constantemente optimizar nuestros procesos y servicios para ser más eficientes, efectivos e innovadores. Asimismo, nos comprometemos a cumplir con los lineamientos establecidos por la ISO/IEC 17025:2017 y a seguir las regulaciones impuestas por entes reguladores y organizaciones que otorgan reconocimiento. Contamos con un equipo altamente capacitado y en constante formación, asegurando la competencia profesional en todas las áreas. Buscamos la mejora continua, integrando soluciones avanzadas para optimizar nuestros procesos y servicios. Mantenemos la imparcialidad, confidencialidad e integridad en nuestras operaciones. Nuestro compromiso es ofrecer resultados confiables, precisos en constante mejora, consolidándonos como líderes en la industria.")}
+            </p>
 
         </div>
           <div className="lg:absolute relative md:end-0 lg:-end-6 md:-bottom-[14px] mx-auto">
