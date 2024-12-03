2306855b66477dcfdc9547197c1b3e50ce387c8f
Andres Felipe Rodriguez roa <89116331+japipe05@users.noreply.github.com>
2024-12-03 07:36:12 -0500

    fase2

--- app/components/landingpage/banner/banner.tsx
+++ app/components/landingpage/banner/banner.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import React,{ useState }  from "react";
+import React,{ useState }  from "react"; 
 import bannerWidget1 from "/public/images/landingpage/background/banner-widget-c.jpeg";
 import bannerWidget2 from "/public/images/landingpage/background/banner-widget-b.jpeg";
 import bannerWidget3 from "/public/images/landingpage/background/banner-widget-e.jpeg";
